9a3e03fd577bc8b6f44908e42df04e8ee7bfa562
ahong <710891234@qq.com>
2017-11-28 11:34:47 +0800

    搭建vue框架中的vuex

---
+++ src/page/login/login.vue
@@ -0,0 +1,15 @@
+<template lang="zh">
+
+  <div class="login_outer">
+    
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+//
+/*
+*
+*懒加载方式code Splitting
+*
+ */
+const HelloWorld = r => require.ensure([], () => r(require('@/components/HelloWorld')), 'HelloWorld')
 
 Vue.use(Router)
 
